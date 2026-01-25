6b5b90915b322ef86cb3871b381ce32036487d61
VijayKumar-028 <valivetivijaykumar05@gmail.com>
2026-01-25 15:57:36 +0530

    Created the CBV for the employees model

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # API URL paths for the student App
     path("students/", views.studentsView),
     path("students/<int:pk>", views.StudentDetailView),
+    # API URL paths for the Employee App
+    path("employees/", views.Employees.as_view()),
+    path("employees/<int:pk>", views.EmployeeDetailed.as_view()),
 ]
